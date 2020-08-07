cc9c178029cbfdb8f757e98d76e8611daff7ab3a
Barbara Moore <barbaraloismoore@gmail.com>
2020-08-07 12:09:08 -0400

    finished oop1 and oop2 MVP

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class is Vehicle
+class Vehicle:
+    pass
+
+# These two inherit from Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+# These two inherit from GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# These two inherit from FlightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
